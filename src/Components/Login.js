219da25ce50066ce9dc79fb4c34fb7f254219363
Camila Fragoso <camilafragoso.98@hotmail.com>
2020-06-14 20:49:20 -0300

    Atualização do Main

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,7 +5,7 @@ import './Login.css';
 const Login = () => {
     return (
         <div>
-            <header>
+            <header className="header">
                 <img className="logo"
                 src="https://66.media.tumblr.com/58a922a53e0b7173e3a71414a5da6d19/07849764433ec5c0-35/s540x810/cb3990785fae4d6fa64be61c88ed8f8d3c1f4753.png"/>
             </header>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import './Login.css';
+import './Main.css';
 
 const Main = () => {
     return (
-        <div>
-            <header>
-                <img className="logo"
-                src="https://66.media.tumblr.com/58a922a53e0b7173e3a71414a5da6d19/07849764433ec5c0-35/s540x810/cb3990785fae4d6fa64be61c88ed8f8d3c1f4753.png"/>
-            </header>
+        <div className="overallmain">
             <div className="bodydiv">
-                <h2>Pressione para iniciar</h2>
-                <button className="iniciar">Iniciar</button>
+                <div className="title">
+                    <h1 className="title1">Bem vindo,</h1>
+                    <h1 className="title2">Roberto</h1>
+                    <h3>Pressione para iniciar</h3>
+                </div>
+                
+                <button className="iniciar">
+                    <img className="icon" src="https://66.media.tumblr.com/9abd5c88fad0e64ec887e717dbe9f27b/293a47036c50ea80-d4/s250x400/f90a87b2bfb6b65f0e78b7e354a5e77411e91179.png"/>
+                </button>
             </div>
         </div>
     );
